5437941302c6dc21cc938e856db5c461d19b9e0b
Leela Mohan <lmohan@salesforce.com>
2020-07-18 12:08:32 +0530

    make it better

--- app.js
+++ app.js
@@ -4,38 +4,45 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 
 var q = 'tasks';
-var url = process.env.CLOUDAMQP_URL || "amqp://localhost";
+var url = process.env.CLOUDAMQP_URL || 'amqp://localhost';
 var open = require('amqplib').connect(url);
 
-app.get('/consume', function(req, res) {
-// Consumer
-open.then(function(conn) {
-  var ok = conn.createChannel();
-  ok = ok.then(function(ch) {
-    ch.assertQueue(q);
-    ch.consume(q, function(msg) {
-      if (msg !== null) {
-        console.log(msg.content.toString());
-        ch.ack(msg);
-      }
-    });
-  });
-  return ok;
-}).then(null, console.warn);
+app.get('/consume', function (req, res) {
+	// Consumer
+	open
+		.then(function (conn) {
+			var ok = conn.createChannel();
+			ok = ok.then(function (ch) {
+				ch.assertQueue(q);
+				ch.consume(q, function (msg) {
+					if (msg !== null) {
+						console.log(msg.content.toString());
+						ch.ack(msg);
+						res.send(msg.content.toString());
+					}
+				});
+			});
+			return ok;
+		})
+		.then(null, console.warn);
 });
 
+app.get('/publish', function (req, res) {
+	// Publisher
+	open
+		.then(function (conn) {
+			var ok = conn.createChannel();
+			ok = ok.then(function (ch) {
+				ch.assertQueue(q);
+				ch.sendToQueue(q, new Buffer.alloc('Request 1 by Leela Mohan'));
+			});
+			return ok;
+		})
+		.then(null, console.warn);
+});
 
-app.get('/', function(req, res) {
-// Publisher
-open.then(function(conn) {
-  var ok = conn.createChannel();
-  ok = ok.then(function(ch) {
-    ch.assertQueue(q);
-    ch.sendToQueue(q, new Buffer('something to do'));
-  });
-  return ok;
-}).then(null, console.warn);
-res.sendFile(path.join(__dirname + '/index.html'));
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
