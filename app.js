0659d01c01b9ce8b4c21d1e1ac6ee443e652dee5
Leela Mohan <lmohan@salesforce.com>
2020-07-18 16:46:10 +0530

    make it better

--- app.js
+++ app.js
@@ -1,4 +1,111 @@
+const path  = require('path');
 const express = require('express');
+const app = express();
+const http = require('http');
+const bodyParser = require('body-parser');
+const amqp = require('amqplib');
+
+// Middleware
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+// simulate request ids
+let lastRequestId = 1;
+
+// RabbitMQ connection string
+const messageQueueConnectionString = process.env.CLOUDAMQP_URL;
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+// handle the request
+app.post('/publish', async function (req, res) {
+  // save request id and increment
+  let requestId = lastRequestId;
+  lastRequestId++;
+
+  // connect to Rabbit MQ and create a channel
+  let connection = await amqp.connect(messageQueueConnectionString);
+  let channel = await connection.createConfirmChannel();
+
+  // publish the data to Rabbit MQ
+  let requestData = req.body.data;
+  console.log("Published a request message, requestId:", requestId);
+  await publishToChannel(channel, { routingKey: "request", exchangeName: "processing", data: { requestId, requestData } });
+
+  // send the request id in the response
+  res.send({ requestId })
+});
+
+// utility function to publish messages to a channel
+function publishToChannel(channel, { routingKey, exchangeName, data }) {
+  return new Promise((resolve, reject) => {
+    channel.publish(exchangeName, routingKey, Buffer.from(JSON.stringify(data), 'utf-8'), { persistent: true }, function (err, ok) {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve();
+    })
+  });
+}
+
+
+async function listenForResults() {
+  // connect to Rabbit MQ
+  let connection = await amqp.connect(messageQueueConnectionString);
+
+  // create a channel and prefetch 1 message at a time
+  let channel = await connection.createChannel();
+  await channel.prefetch(1);
+
+  // start consuming messages
+  await consume({ connection, channel });
+}
+
+
+// consume messages from RabbitMQ
+function consume({ connection, channel, resultsChannel }) {
+  return new Promise((resolve, reject) => {
+    channel.consume("processing.results", async function (msg) {
+      // parse message
+      let msgBody = msg.content.toString();
+      let data = JSON.parse(msgBody);
+      let requestId = data.requestId;
+      let processingResults = data.processingResults;
+      console.log("Received a result message, requestId:", requestId, "processingResults:", processingResults);
+
+      // acknowledge message as received
+      await channel.ack(msg);
+    });
+
+    // handle connection closed
+    connection.on("close", (err) => {
+      return reject(err);
+    });
+
+    // handle errors
+    connection.on("error", (err) => {
+      return reject(err);
+    });
+  });
+}
+
+// Start the server
+const PORT =  process.env.PORT || 3000;
+server = http.createServer(app);
+server.listen(PORT, "localhost", function (err) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.info("Listening on port %s.", PORT);
+  }
+});
+
+// listen for results on RabbitMQ
+listenForResults();
+
+/*const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
@@ -23,7 +130,8 @@ app.get('/consume', function (req, res) {
 					if (msg !== null) {
 						console.log(msg.content.toString());
 						ch.ack(msg);
-						res.send(msg.content.toString());
+            res.send(msg.content.toString());
+            
           }
 				});
 			});
@@ -51,4 +159,5 @@ app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(port, () => console.log(`listening on port ${port}!`));
+app.listen(port, () => console.log(`listening on port ${port}!`)); */
+
