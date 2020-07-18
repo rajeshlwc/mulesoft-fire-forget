edc6b94f0bed2d18cd9b59801002f9389cbe5d0a
Leela Mohan <lmohan@salesforce.com>
2020-07-18 14:57:15 +0530

    make it better

--- public/assets/scripts/index.js
+++ public/assets/scripts/index.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
   
   $.ajax({
 		type: 'POST',
-		data: JSON.stringify("Data Entered!!"),
+		data: "Data Entered!!",
 		contentType: 'application/json',
 		url: '/publish',
 		success: function (data) {
 			console.log('success');
-			console.log(JSON.stringify(data));
+			console.log(data);
 		},
 	});
 });
