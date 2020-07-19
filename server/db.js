993c24f15687bcfe94f8ddf70a609ebfeec29f30
Leela Mohan <lmohan@salesforce.com>
2020-07-19 22:47:30 +0530

    make it better

--- server/db.js
+++ server/db.js
@@ -59,7 +59,7 @@ const insertMessage = (message) => {
   console.log(msg.source);
   console.log(msg.message);
 
-  const insertMsg = `Insert into rabbit_queue values ('Hello Leela!', 'Heroku')`;
+  const insertMsg = `Insert into rabbit_queue values ('${msg.message}', '${msg.source}')`;
 
 return pool.connect()
       .then((client) => {
